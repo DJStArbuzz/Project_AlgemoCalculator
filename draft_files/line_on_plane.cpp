8607fb9fd4b00225f2507bc47c6171b094de35c9
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2023-11-23 22:05:32 +0300

    Update line_on_plane.cpp

--- draft_files/line_on_plane.cpp
+++ draft_files/line_on_plane.cpp
@@ -12,6 +12,10 @@ const float PI = acos(-1);
 
 using namespace std;
 
+void theory() {
+
+}
+
 // Процедура поиска угла наклона
 // Пункт 1
 void search_for_the_angular_coefficient() {
@@ -25,17 +29,17 @@ void search_for_the_angular_coefficient() {
 		b - отсекаемый отрезок на оси Oy.
 	*/
 
-	double tan_tmp, atan_tmp; 
+	double tan_tmp, atan_tmp;
 	/*
 		tan_tmp - угловой коэффициент
 
 		y = kx + b, где k = tg(a),
 		a - угол наклона к оси Ox.
-	
+
 	*/
 
 	cout << "Введите параметры y и x и отрезок, отсекаемый на оси Оу.\n" << endl;
-	
+
 	cout << "Для y: ";
 	cin >> tmp;
 
@@ -56,10 +60,10 @@ void search_for_the_angular_coefficient() {
 	cout << endl;
 }
 
-// Составление прямой через одну некоторую точку
+// Составление уравнения прямой через одну некоторую точку
 // Пункт 2
-void drawing_up_the_equation() {
-	/* 
+void drawing_up_the_equation_one_point() {
+	/*
 		y - y0 = k * (x - x0)
 		y = kx - kx0 + y0
 
@@ -69,7 +73,7 @@ void drawing_up_the_equation() {
 	*/
 
 	pair <double, double> coordinates;
-	/* 
+	/*
 		Переменная, собирающая координаты точки.
 		first - абсцисса,
 		second - ордината.
@@ -77,8 +81,52 @@ void drawing_up_the_equation() {
 	cout << "Введите координаты некоторой точки: ";
 	cin >> coordinates.first >> coordinates.second;
 
+	cout << "Уравнение прямой: y = k * x - k * " << coordinates.first;
+	cout << " + " << coordinates.second << endl;
+	cout << endl;
+}
 
+void drawing_up_the_equation_two_point() {
+	/*
+		x - x1    y - y1
+		------- = ------- 
+		x2 - x1   y2 - y1
 
+		(x - x1)*(y2 - y1) = (x2 - x1)*(y - y1)
+		x * (y2 - y1) - x1 * (y2 - y1) = y * (x2 - x1) - y1 * (x2 - x1)
+
+		delta() = d()
+		Пусть d(y) = y2 - y1, d(x) = x2 - x1
+
+		x * d(y) - x1 * d(y) = y * d(x) - y1 * d(x)
+		x * d(y) - x1 * d(y) + y1 * d(x) = y * d(x) | :(d(x)
+
+		y = (d(y) / d(x)) * x + (-x1 * d(y) + y1 * d(x)) / d(x)
+	*/
+
+	pair <double, double> coordinates_first;
+	pair <double, double> coordinates_second;
+	/*
+		Переменные, собирающие координаты точки.
+		first - абсцисса,
+		second - ордината.
+	*/
+
+	cout << "Введите координаты первой некоторой точки: ";
+	cin >> coordinates_first.first >> coordinates_first.second;
+
+	cout << "Введите координаты второй некоторой точки: ";
+	cin >> coordinates_second.first >> coordinates_second.second;
+
+	double delta_x = coordinates_second.first - coordinates_first.first;
+	double delta_y = coordinates_second.second - coordinates_first.second;
+	
+	double div_delta = delta_y / delta_x;
+	double tmp = (coordinates_first.second * delta_x - coordinates_first.first * delta_y) / delta_x;
+
+	cout << "Уравнение прямой: y = x * " << div_delta;
+	cout << " + (" << tmp << ")" << endl;
+	cout << endl;
 }
 
 // Процедура выхода из программы
@@ -104,7 +152,7 @@ void menu() {
 // Процедура выбора операции
 void choose_number_of_problem() {
 	menu();
-	
+
 	int number_of_problem;  // Выбор вопроса
 	cin >> number_of_problem;
 
@@ -112,12 +160,17 @@ void choose_number_of_problem() {
 
 	switch (number_of_problem)
 	{
+	case(0):
+		theory();
+		break;
 	case(1):
 		search_for_the_angular_coefficient();
 		break;
 	case(2):
+		drawing_up_the_equation_one_point();
 		break;
 	case(3):
+		drawing_up_the_equation_two_point();
 		break;
 	case(4):
 		break;
